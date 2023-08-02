1f86ed8598a19066afadf8214968c261dbb999ea
Lily <lamb39459@gmail.com>
2023-08-02 16:56:35 +0800

    測試刪除

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -9,14 +9,16 @@ import web.member.pojo.Member;
 
 public class TestApp {
 	public static void main(String[] args) {
-		Member member = new Member();
-		member.setUsername("使用者名稱");
-		member.setPassword("密碼");
-		member.setNickname("暱稱");
-		
+//		Member member = new Member();
+//		member.setUsername("使用者名稱");
+//		member.setPassword("密碼");
+//		member.setNickname("暱稱");
+//		
 		TestApp app = new TestApp();
-		app.insert(member);
-		System.out.println(member.getId());
+//		app.insert(member);
+//		System.out.println(member.getId());
+		
+		System.out.println(app.deleteById(3));
 	}
 	
 	public Integer insert(Member member) {
@@ -31,7 +33,22 @@ public class TestApp {
 			e.printStackTrace();
 			session.getTransaction().rollback();
 			return null;
-		}
-		
+		}	
+	}
+	
+	public Integer deleteById(Integer id) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member member = session.get(Member.class, id);
+			session.remove(member);
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;
+		}	
 	}
 }
