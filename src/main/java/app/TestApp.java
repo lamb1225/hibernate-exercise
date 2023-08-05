f3514d93f18b5f758ed4cae1f218cbd6e7c19f5e
Lily <lamb39459@gmail.com>
2023-08-05 09:50:28 +0800

    測試修改

--- src/main/java/app/TestApp.java
+++ src/main/java/app/TestApp.java
@@ -14,13 +14,23 @@ public class TestApp {
 //		member.setPassword("密碼");
 //		member.setNickname("暱稱");
 //		
+		// 新增測試
 		TestApp app = new TestApp();
 //		app.insert(member);
 //		System.out.println(member.getId());
 		
-		System.out.println(app.deleteById(3));
+		// 刪除測試
+//		System.out.println(app.deleteById(3));
+		
+		// 修改測試
+		Member member = new Member();
+		member.setId(1);
+		member.setPass(false);
+		member.setRoleId(2);
+		System.out.println(app.updateById(member));
 	}
 	
+	// 新增
 	public Integer insert(Member member) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
@@ -36,6 +46,7 @@ public class TestApp {
 		}	
 	}
 	
+	// 刪除
 	public Integer deleteById(Integer id) {
 		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
 		Session session = sessionFactory.openSession();
@@ -43,6 +54,30 @@ public class TestApp {
 			Transaction transaction = session.beginTransaction();
 			Member member = session.get(Member.class, id);
 			session.remove(member);
+			transaction.commit();
+			return 1;
+		} catch (Exception e) {
+			e.printStackTrace();
+			session.getTransaction().rollback();
+			return -1;
+		}	
+	}
+	
+	// 修改
+	public Integer updateById(Member newMember) {
+		SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+		Session session = sessionFactory.openSession();
+		try {
+			Transaction transaction = session.beginTransaction();
+			Member oldMember = session.get(Member.class, newMember.getId());
+			final Boolean pass = newMember.getPass();
+			if(pass != null) {
+				oldMember.setPass(pass);
+			}
+			final Integer roleId = newMember.getRoleId();
+			if(roleId != null) {
+				oldMember.setRoleId(roleId);
+			}
 			transaction.commit();
 			return 1;
 		} catch (Exception e) {
